6831bbe25d311ece96f7c3a34e24d1bba6223cb1
kwankat <katelyn.wankat@gmail.com>
2022-03-02 11:37:13 -0600

    update

--- Unit2/multisound/sketch.js
+++ Unit2/multisound/sketch.js
@@ -5,46 +5,67 @@ function preload() {
   song1 = loadSound("music/song1.mp3");
   song2 = loadSound("music/song2.mp3");
   song3 = loadSound("music/song3.mp3");
+
+  song1. loop();
+  song1. pause();
+  song2. loop();
+  song2. pause();
+  song3. loop();
+  song3. pause();
+
 }
 
 function setup() {
   createCanvas(500, 500)
+
 }
 
 function draw() {
 
   switch (state) {
-    case :0
+    case 0:
+    background('red');
     song1.play();
     state = 1;
     break;
 
-    case :1
+    case 1:
     background('red');
-    text("listening to some sick beats", 200, 200)
+    text("listening to some sick beats", 200, 200);
     break;
 
-    case :2
+    case 2:
     background('orange');
-    text("NICE", 200, 200)
+    text("NICE", 200, 200);
+    song2.play();
+    state = 3 ;
     break;
 
-    case :3
-    background('yellow');
-    text("JAMMIN", 200, 200)
+    case 3:
+    background('orange');
+    text("JAMMIN", 200, 200);
+
     break;
 
-    case :4
+    case 4:
     background('pink');
-    text("JAMMIN", 200, 200)
+    song3.play();
+    state = 5;
+    break;
+
+    case 5:
+    background('pink');
+    text("CATJAM", 200, 200);
     break;
 
   }
 }
 
 function mouseReleased() {
-  state = (state + 1) % 6;
   song1.pause();
   song2.pause();
   song3.pause();
+
+  state++;
+  if(state > 5) state = 0;
 }
--- Unit2/stoplight/sketch.js
+++ Unit2/stoplight/sketch.js
@@ -1,5 +1,7 @@
-let state = 0
-let timer = 0
+let state = 0;
+let timer = 0;
+let x = 0;
+let velocity = 5 ;
 
 function setup() {
   createCanvas(800, 800);
@@ -13,40 +15,55 @@ function draw() {
   fill('yellow');
   rect(width / 2, height / 2, 200, 700);
 
+fill('orange');
+rect(x,750,75,20);
+x = x + velocity ;
+if (x > width) {
+  x = 0;
+}
+
 
 switch (state) {
 
 
   case 0: //red light
     fill('red');
-    ellipse(width / 2, height / 2, 150, 150);
-    fill('grey');
     ellipse(width / 2, height / 2 - 170, 150, 150);
+    fill('grey');
+    ellipse(width / 2, height / 2, 150, 150);
     ellipse(width / 2, height / 2 + 170, 150, 150)
+    velocity = 0;
     break;
 
   case 1: //yellow light
-    fill('yellow');
-    ellipse(width / 2, height / 2, 150, 150);
     fill('grey');
     ellipse(width / 2, height / 2 - 170, 150, 150);
+    ellipse(width / 2, height / 2, 150, 150);
+    fill('green');
     ellipse(width / 2, height / 2 + 170, 150, 150)
+    velocity = 5;
     break;
 
   case 2: //green light
-    fill('green');
-    ellipse(width / 2, height / 2, 150, 150);
+    fill('grey');
     ellipse(width / 2, height / 2 - 170, 150, 150);
+    fill('yellow');
+    ellipse(width / 2, height / 2, 150, 150);
     fill('grey');
     ellipse(width / 2, height / 2 + 170, 150, 150)
+    velocity = 2.5;
     break;
   }
-  timer = timer + 1
-  if (timer > 100) {
-    timer = 0 ;
-  }
+
+timer = timer + 1 ;
+if (timer > 100) {
+  timer = 0;
+  state ++;
+  if (state > 2) state = 0;
+}
+
+}
   function mouseReleased() {
-    state++;
+    state ++;
     if (state > 2) state = 0;
   }
-}
